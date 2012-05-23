499df7e540530e3a7020be8d36f16164bf21a1c4
Jose Antonio Pio Gil <josetonyp@latizana.com>
2012-05-23 22:02:37 +0200

    Loding files libraries in order

--- lib/kikubari.rb
+++ lib/kikubari.rb
@@ -1,36 +1,36 @@
 module Kikubari
-  
+
   class Deploy
-  
+
     attr_accessor :config
-  
+
     def initialize ( configuration  )
       @config = configuration
       return rollback if @config.rollback
       deploy
     end
-    
+
     def deploy
       deployer = get_deployer( @config )
       deployer.create_deploy_structure.deploy
     end
-  
+
     def rollback
       p "rollingback"
     end
-  
+
     def change( version )
       p "changing to version #{version}"
     end
-  
-    private 
-  
+
+    private
+
     def get_deployer config
-      deployer_class = config.get_deployer_class  
-      eval(deployer_class).new config  
+      deployer_class = config.get_deployer_class
+      eval(deployer_class).new config
     end
-  
+
 
   end
-  
+
 end
